0de32cb9379eb35e16ec9c2eb4942b04062830a3
rhouthuijzen <r.houthuijzen@tools4ever.com>
2023-10-24 14:12:18 +0200

    Fix: alt shift f + version

--- revoke.ps1
+++ revoke.ps1
@@ -1,7 +1,7 @@
 ####################################################
 # HelloID-Conn-Prov-Target-Zivver-Entitlement-Revoke
 #
-# Version: 1.0.0
+# Version: 1.1.0
 ####################################################
 # Initialize default values
 $config = $configuration | ConvertFrom-Json
@@ -53,14 +53,15 @@ function Invoke-ZivverRestMethod {
             ContentType = $ContentType
         }
 
-        if ($Body){
+        if ($Body) {
             Write-Verbose 'Adding body to request'
             $utf8Encoding = [System.Text.Encoding]::UTF8
             $encodedBody = $utf8Encoding.GetBytes($body)
             $splatParams['Body'] = $encodedBody
         }
         Invoke-RestMethod @splatParams -Verbose:$false
-    } catch {
+    }
+    catch {
         $PSCmdlet.ThrowTerminatingError($_)
     }
 }
@@ -83,7 +84,8 @@ function Resolve-ZivverError {
             $errorDetails = $ErrorObject.ErrorDetails.Message
             $httpErrorObj.ErrorDetails = "Exception: $($ErrorObject.Exception.Message), Error: $($errorDetails)"
             $httpErrorObj.FriendlyMessage = "Error: $($errorDetails)"
-        } catch {
+        }
+        catch {
             $httpErrorObj.FriendlyMessage = "Received an unexpected response. The JSON could not be converted, error: [$($_.Exception.Message)]. Original error from web service: [$($ErrorObject.Exception.Message)]"
         }
         Write-Output $httpErrorObj
@@ -99,7 +101,7 @@ try {
     }
 
     Write-Verbose 'Creating authorization header'
-    $headers = [System.Collections.Generic.Dictionary[[String],[String]]]::new()
+    $headers = [System.Collections.Generic.Dictionary[[String], [String]]]::new()
     $headers.Add("Authorization", "Bearer $($config.Token)")
     $splatParams = @{
         Headers = $headers
@@ -124,6 +126,7 @@ try {
     if ($responseUser.Length -lt 1) {
         throw "Zivver account for: [$($p.DisplayName)] not found. Possibly deleted"
     }
+
     $splatParams['Endpoint'] = "Groups/$($pRef.Reference)"
     $splatParams['Method'] = 'GET'
     $responseGroup = Invoke-ZivverRestMethod @splatParams
@@ -208,7 +211,8 @@ try {
             }
         }
     }
-} catch {
+}
+catch {
     $success = $false
     $ex = $PSItem
     if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
@@ -216,7 +220,8 @@ try {
         $errorObj = Resolve-ZivverError -ErrorObject $ex
         $auditMessage = "Could not revoke Zivver account. Error: $($errorObj.FriendlyMessage)"
         Write-Verbose "Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
-    } else {
+    }
+    else {
         $auditMessage = "Could not revoke Zivver account. Error: $($ex.Exception.Message)"
         Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
     }
@@ -224,8 +229,9 @@ try {
             Message = $auditMessage
             IsError = $true
         })
-# End
-} finally {
+    # End
+}
+finally {
     $result = [PSCustomObject]@{
         Success   = $success
         Auditlogs = $auditLogs
